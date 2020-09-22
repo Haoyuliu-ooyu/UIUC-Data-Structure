f6591c6a9117272347588a8f435f791fc71f77d8
Haoyu Liu <haoyul4@linux-a2.ews.illinois.edu>
2020-09-21 21:04:09 -0500

    mp

--- mp_stickers/main.cpp
+++ mp_stickers/main.cpp
@@ -8,7 +8,23 @@ int main() {
   //   Before exiting main, save your creation to disk as myImage.png
   //
 
-  
+  Image base;
+  base.readFromFile("alma.png");
+  StickerSheet sheet = StickerSheet(base, 15);
+  Image sticker;
+  sticker.readFromFile("i.png");
+  Image frog;
+  frog.readFromFile("frog.png");
+  Image cover;
+  cover.readFromFile("lighten.png");
+  cover.resize(150, 200);
+  for (unsigned i = 0; i < 5; i++) {
+    sheet.addSticker(sticker, 50 * i, 30 * i);
+    sheet.addSticker(frog, 300 - 8*i, 200 + 40*i);
+    sheet.addSticker(cover, 20 + 10*i, 20 + 36*i);
+  }
+  sheet.render().writeToFile("myImage.png");
+
 
   return 0;
 }
