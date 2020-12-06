e90a20560eac3d941b894418cd355d99bbcbf18d
Haoyuliu-ooyu <haoyul4@illinois.edu>
2020-12-06 17:24:51 -0600

    finished

--- lab_flow/NetworkFlow.cpp
+++ lab_flow/NetworkFlow.cpp
@@ -22,6 +22,21 @@ NetworkFlow::NetworkFlow(Graph & startingGraph, Vertex source, Vertex sink) :
   g_(startingGraph), residual_(Graph(true,true)), flow_(Graph(true,true)), source_(source), sink_(sink) {
 
   // YOUR CODE HERE
+  auto vertices = g_.getVertices();
+  auto edges = g_.getEdges();
+  for (auto v : vertices) {
+    residual_.insertVertex(v);
+    flow_.insertVertex(v);
+  }
+  for (auto e : edges) {
+    residual_.insertEdge(e.source, e.dest);
+    residual_.setEdgeWeight(e.source, e.dest, e.getWeight());
+    residual_.insertEdge(e.dest, e.source);
+    residual_.setEdgeWeight(e.dest, e.source, 0);
+    flow_.insertEdge(e.source, e.dest);
+    flow_.setEdgeWeight(e.source, e.dest, 0);
+  }
+
 }
 
   /**
@@ -84,7 +99,17 @@ bool NetworkFlow::findAugmentingPath(Vertex source, Vertex sink, std::vector<Ver
 
 int NetworkFlow::pathCapacity(const std::vector<Vertex> & path) const {
   // YOUR CODE HERE
-  return 0;
+  int min = 999999999;
+  vector<int> temp;
+  for (unsigned i = 0; i < path.size() - 1; i++) {
+    temp.push_back(residual_.getEdgeWeight(path[i], path[i+1]));
+  }
+  for (auto t : temp) {
+    if (t <= min) {
+      min = t;
+    }
+  }
+  return min;
 }
 
   /**
@@ -97,6 +122,23 @@ int NetworkFlow::pathCapacity(const std::vector<Vertex> & path) const {
 
 const Graph & NetworkFlow::calculateFlow() {
   // YOUR CODE HERE
+  vector<Vertex> vertices;
+  int max_flow = 0;
+  while (findAugmentingPath(source_, sink_, vertices)) {
+    auto edges = residual_.getEdges();
+    int temp_weight = pathCapacity(vertices);
+    max_flow += temp_weight;
+    for (unsigned i = 0; i < vertices.size() - 1; i++) {
+      residual_.setEdgeWeight(vertices[i], vertices[i+1], residual_.getEdgeWeight(vertices[i], vertices[i+1]) - temp_weight);
+      residual_.setEdgeWeight(vertices[i+1], vertices[i], residual_.getEdgeWeight(vertices[i+1], vertices[i]) + temp_weight);
+      if (flow_.edgeExists(vertices[i], vertices[i+1])) {
+        flow_.setEdgeWeight(vertices[i], vertices[i+1], flow_.getEdgeWeight(vertices[i], vertices[i+1]) + temp_weight);
+      } else {
+        flow_.setEdgeWeight(vertices[i+1], vertices[i], flow_.getEdgeWeight(vertices[i+1], vertices[i]) - temp_weight);
+      }
+    }
+  }
+  maxFlow_ = max_flow;
   return flow_;
 }
 
