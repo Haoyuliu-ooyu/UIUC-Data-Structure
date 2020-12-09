05272e75f3aa5e0110b2a0c1427bf1cc3c02ff70
Haoyuliu-ooyu <haoyul4@illinois.edu>
2020-12-08 23:49:20 -0600

    regrade

--- mp_mazes/maze.cpp
+++ mp_mazes/maze.cpp
@@ -1,1 +1,21 @@
 /* Your code here! */
+#include<vector>
+
+#include "maze.h"
+
+SquareMaze::SquareMaze() {}
+
+bool SquareMaze::canTravel(int x, int y, int dir) const {return true;}
+
+PNG * SquareMaze::drawMaze(	) const {return new PNG();}
+
+PNG * SquareMaze::drawMazeWithSolution() {return new PNG();}
+
+void SquareMaze::makeMaze(int width, int height) {}
+
+void SquareMaze::setWall(int x, int y, int dir, bool exists) {}
+
+std::vector<int> SquareMaze::solveMaze() {
+    std::vector<int> to_return;
+    return to_return;
+}
--- mp_mazes/maze.h
+++ mp_mazes/maze.h
@@ -1,1 +1,20 @@
 /* Your code here! */
+
+
+
+#include "cs225/PNG.h"
+
+using namespace cs225;
+
+class SquareMaze 
+{
+    public:
+        SquareMaze();
+        bool canTravel(int x, int y, int dir) const;
+        PNG * drawMaze(	) const;
+        PNG * drawMazeWithSolution();
+        void makeMaze(int width, int height);
+        void setWall(int x, int y, int dir, bool exists );
+        std::vector< int > solveMaze();
+};
+
